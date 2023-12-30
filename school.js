a8d67fe458fc2a7301e04e5f78a4762cdff3bd1f
SamikshaChougale <141617932+SamikshaChougale@users.noreply.github.com>
2023-12-30 10:34:05 +0530

    Add files via upload

--- school.js
+++ school.js
@@ -13,5 +13,31 @@ var swiper = new Swiper(".mySwiper", {
     navLinks.style.right = "0";
   }
   function hideMenu(){
-    navLinks.style.right = "-1000px";
+    navLinks.style.right = "-200px";
   }
+
+/*--Pop up function for media--*/
+// Function to open the popup with the selected image
+function openPopup(imageSrc) {
+  document.getElementById("popupImg").src = imageSrc;
+  document.getElementById("popup").style.display = "flex";
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+// Add event listeners to all "Explore" buttons
+document.querySelectorAll(".button").forEach((button) => {
+  button.addEventListener("click", function(event) {
+      event.preventDefault();
+      const imageSrc = this.getAttribute("data-image");
+      openPopup(imageSrc);
+  });
+});
+
+
+
+
+
